acdb0fe4fa111454df787ea17aadaf57f0d0b0a1
Lee Jeong Hoon <xorolx297@naver.com>
2019-08-15 22:14:21 +0900

    make main view frame

--- app/src/main/java/com/pingpong/moamoa/MainActivity.kt
+++ app/src/main/java/com/pingpong/moamoa/MainActivity.kt
@@ -2,11 +2,19 @@ package com.pingpong.moamoa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    fun initView() {
+        tabLayout_main.addTab(tabLayout_main.newTab().setText("전체"))
+        tabLayout_main.addTab(tabLayout_main.newTab().setText("고민 중"))
+        tabLayout_main.addTab(tabLayout_main.newTab().setText("구매 완료"))
     }
 }
